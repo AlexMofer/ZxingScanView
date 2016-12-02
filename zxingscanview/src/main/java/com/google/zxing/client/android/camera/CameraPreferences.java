3ffc884680c8e59f82040d1cb2ed98545b3cfc25
Alex <alex@mazing.com>
2016-12-02 18:59:15 +0800

    ZxingScanView 3.3.0 发布

---
+++ zxingscanview/src/main/java/com/google/zxing/client/android/camera/CameraPreferences.java
@@ -0,0 +1,18 @@
+package com.google.zxing.client.android.camera;
+
+/**
+ * Camera settings
+ * TODO
+ * Created by Alex on 2016/11/22.
+ */
+class CameraPreferences {
+
+    static final String KEY_FRONT_LIGHT_MODE = "preferences_front_light_mode";
+    static final String KEY_AUTO_FOCUS = "preferences_auto_focus";
+    static final String KEY_INVERT_SCAN = "preferences_invert_scan";
+
+    static final String KEY_DISABLE_CONTINUOUS_FOCUS = "preferences_disable_continuous_focus";
+    static final String KEY_DISABLE_EXPOSURE = "preferences_disable_exposure";
+    static final String KEY_DISABLE_METERING = "preferences_disable_metering";
+    static final String KEY_DISABLE_BARCODE_SCENE_MODE = "preferences_disable_barcode_scene_mode";
+}
